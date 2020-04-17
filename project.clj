6aab563d6697f7e1cd95d05bf278ee90e9a1ef10
Dan Fornika <dfornika@gmail.com>
2020-04-17 16:26:21 -0700

    Revert change of clojure version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/ontodev/edn-ld"
   :license {:name "BSD 3-Clause License"
             :url "http://opensource.org/licenses/BSD-3-Clause"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.7.0-beta3"]
                  [prismatic/schema "0.4.2"]
                  [org.apache.jena/jena-arq "3.0.1"]
                  [org.codehaus.woodstox/woodstox-core-asl "4.3.0"]]
